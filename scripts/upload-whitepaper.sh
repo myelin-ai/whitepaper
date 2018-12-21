2942ed73216cdf11f12a5a649e3434d73c7e3a8c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-12-21 18:09:57 +0100

    Use correct environment variable

--- scripts/upload-whitepaper.sh
+++ scripts/upload-whitepaper.sh
@@ -2,5 +2,5 @@
 
 set -e +x /bin/bash
 
-scp whitepaper.json "$DOCS_SCP_UPLOAD_TARGET"
-scp main.pdf "$DOCS_SCP_UPLOAD_TARGET/whitepaper.pdf"
+scp whitepaper.json "$WHITEPAPER_SCP_UPLOAD_TARGET"
+scp main.pdf "$WHITEPAPER_SCP_UPLOAD_TARGET/whitepaper.pdf"
