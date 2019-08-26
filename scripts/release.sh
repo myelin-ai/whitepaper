188c51bc1e07cff569c83608241340149c3db55d
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-08-26 10:29:26 +0200

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+source_file='main.pdf'
+bot_user='myelin-bot'
+target_repository="https://$bot_user:$GITHUB_TOKEN@github.com/whitepaper.myelin.ch.git"
+temp_directory=$(mktemp -d)
+target_file="$temp_directory/whitepaper.pdf"
+
+git clone --quiet --shallow \
+    "$target_repository" "$temp_directory"
+
+cp "$source_file" "$target_file"
+
+git -C "$temp_directory" \
+    commit --allow-empty -m "Build for $GITHUB_SHA"
+
+git -C "$temp_directory" \
+    push
